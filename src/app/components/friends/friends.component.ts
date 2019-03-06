c9c2f130db08590864db79d3cebb93b9570de871
Sofía Martín <sofimrtn@gmail.com>
2019-03-06 17:18:34 +0100

    Functions for getting friends and friends names.

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RdfService } from '../../services/rdf.service';
-
+import { Friend } from '../../models/friend.model';
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
@@ -8,23 +8,40 @@ import { RdfService } from '../../services/rdf.service';
 })
 export class FriendsComponent implements OnInit {
 
+  mi_listado_de_friends: Friend[] = [];
+
   constructor(private rdf: RdfService) { }
 
   ngOnInit() {
     this.loadFriends();
   }
-
+  /*
+  get user's friends from rdf.
+   */
   async loadFriends() {
+    let list_friends;
     try {
-      const list_friends = await this.rdf.getFriends(); //devuelve un array de urls
+      list_friends = await this.rdf.getFriends(); //devuelve un array de urls
+      console.log(list_friends);
       if (list_friends) {
         for (let i = 0; i < list_friends.length; i++) {
-          return list_friends[i];
+          const names = this.parseURL(list_friends[i]);
+          const amigo: Friend = {name: names, url: list_friends[i]};
+          this.mi_listado_de_friends.push(amigo);
         }
       }
     } catch (error) {
       console.log(`Error: ${error}`);
     }
   }
+  /*
+  get names from friend's url.
+   */
+  parseURL(url: string): string {
+    const sinHttps = url.replace('https://', '');
+    const name = sinHttps.split('.')[0];
+    return name;
+  }
 
 }
+
