adf7ebe16aa1f9f15bdadded4c9879ae129e954c
UO204115 <UO204115@uniovi.es>
2019-04-01 22:39:21 +0200

    Added test for Inrupt login

--- test/acceptance/steps/login_inrupt.steps.js
+++ test/acceptance/steps/login_inrupt.steps.js
@@ -4,21 +4,15 @@ const { Given, When, Then } = require('cucumber');
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
-When('I am on the main page {string}', function (site) {
+When('I am on the main page to login with Inrupt {string}', function (site) {
     return browser.get(site);
 });
 
-When('I select Solid Community ID provider and click on Go button', function() {
+When('I select Inrupt ID provider and click on Go button', function() {
     var providerLink = element(by.id('provider'));
     return providerLink.click();
-    var solidLink = element(by.id('a63f3defa536'));
+    var solidLink = element(by.id('a3a057f99575'));
     return solidLink.click();
     var goToLink = element(by.id('goLogin'));
     return goToLink.click();
 });
-/*
-When('I click on Go button', function() {
-
-});
-
-*/
---
+++ test/acceptance/steps/login_solid.steps.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+When('I am on the main page to login with Solid Community {string}', function (site) {
+    return browser.get(site);
+});
+
+When('I select Solid Community ID provider and click on Go button', function() {
+    var providerLink = element(by.id('provider'));
+    return providerLink.click();
+    var solidLink = element(by.id('a63f3defa536'));
+    return solidLink.click();
+    var goToLink = element(by.id('goLogin'));
+    return goToLink.click();
+});
+/*
+Then('I am on the page {string}', function(site) {
+    return browser.get(site);
+});
+*/
