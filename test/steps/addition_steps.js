27b3e7fe5828087bc1d33988dfe7156468c9cdb9
Yago Prado <yagoprado@yahoo.es>
2019-04-01 20:03:32 +0200

    Creating first scenario and step

---
+++ test/steps/addition_steps.js
@@ -0,0 +1,17 @@
+const { Before, Given, When, Then } = require('cucumber')
+
+Given('the numbers {int} and {int}', function (int, int2, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
+
+
+When('they are added together', function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
+
+Then('should the result be {int}', function (int, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
