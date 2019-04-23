95f6a104b094a63be019ca3f487759b7ab2a8a53
Yago Prado <yagoprado@yahoo.es>
2019-04-23 02:40:18 +0200

    Adding more tests in chat component

--- src/app/components/chat/chat.component.spec.ts
+++ src/app/components/chat/chat.component.spec.ts
@@ -8,7 +8,24 @@ import {ChatComponent} from './chat.component';
 import {RdfService} from '../../services/rdf.service';
 import {ChatService} from '../../services/chat.service';
 import {AuthService} from '../../services/solid.auth.service';
+import {By} from '@angular/platform-browser';
 
+class MockChatService {
+
+    written = false;
+
+    async write(senderId, ruta, messageContent, user) {
+        this.written = true;
+    }
+
+    async loadFriends(): Promise<string[]> {
+      return ['pepe', 'luis'];
+    }
+
+    initChat(name) {
+
+    }
+}
 
 describe('ChatComponent', () => {
     let component: ChatComponent;
@@ -21,7 +38,16 @@ describe('ChatComponent', () => {
 
         const chatServiceStub = {
             loadFriends: async () => ({}),
-            createBaseFolder: () => ({})
+            write: () => ({})
+        };
+
+        const rdfServiceStub = {
+            getProfile: () => ({}),
+            updateProfile: () => ({})
+        };
+        const authServiceStub = {
+            saveOldUserData: () => ({}),
+            solidSignOut: () => ({})
         };
 
 
@@ -30,7 +56,9 @@ describe('ChatComponent', () => {
         TestBed.configureTestingModule({
             declarations: [ ChatComponent ],
             imports: [ FormsModule, ToastrModule.forRoot()],
-            providers: [{ provide: ChatService, useValue: chatServiceStub },
+            providers: [{ provide: ChatService, useClass: MockChatService},
+                { provide: RdfService, useValue: rdfServiceStub },
+                { provide: AuthService, useValue: authServiceStub }
                ]
         })
             .compileComponents();
@@ -44,6 +72,25 @@ describe('ChatComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('have to contain 2 friends', () => {
+        expect(component.mi_listado_de_friends.length).toBe(2);
+    });
+
+    describe('Send the message', () => {
+
+
+        it('send button', () => {
+            spyOn(component, 'write');
+            const boton = fixture.debugElement.query(By.css('#btn-chat')).nativeElement;
+            boton.click();
+            expect(component.write).toHaveBeenCalledTimes(1);
+
+        });
+
+
+
     });
 
 });
